8757b36d7f89a8f86bc1e9340f371b63054afb1e
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2023-08-09 17:53:51 +0700

    Initial Commit

---
+++ RMITCasino/RMITCasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMITCasinoApp.swift
+//  RMITCasino
+//
+//  Created by Thu Nguyen  on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RMITCasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
